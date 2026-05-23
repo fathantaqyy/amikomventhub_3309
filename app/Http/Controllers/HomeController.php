4abb9e8c0cf8d1b428c09552f69f5cdb4387b964
fathantaqyy <fathantaqy@students.amikom.ac.id>
2026-05-23 22:03:19 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Partner;
-use App\Models\Category;
 
 class HomeController extends Controller
 {
     public function index()
-{
-    dd('HOMECONTROLLER JALAN');
-}
+    {
+        $partners = Partner::latest()->get();
+
+        return view('welcome', compact('partners'));
+    }
 }
