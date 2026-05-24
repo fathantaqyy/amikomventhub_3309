47dfd520454fc951b7c2f7bb015962d5fe73cca6
fathantaqyy <fathantaqy@students.amikom.ac.id>
2026-05-24 21:10:00 +0700

    Update database.php
    Fix: Database connection for cloud.

--- config/database.php
+++ config/database.php
@@ -96,7 +96,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'sslmode' => 'require',
+            'options' => [
+                'endpoint' => 'ep-sweet-voice-aoh0j8hk',
+            ],
         ],
 
         'sqlsrv' => [
