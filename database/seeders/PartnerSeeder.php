eb02958016514150f458d42a7d129fbae2548835
fathantaqyy <fathantaqy@students.amikom.ac.id>
2026-05-24 23:49:50 +0700

    Update PartnerSeeder.php

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Partner; // Pastikan model dipanggil
 
 class PartnerSeeder extends Seeder
 {
@@ -12,6 +13,36 @@ class PartnerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Daftar partner yang ingin dimasukkan
+        $partners = [
+            [
+                'name' => 'Amikom Yogyakarta',
+                'logo_url' => 'https://via.placeholder.com/150?text=Amikom', // Ganti dengan path gambarmu misal: asset('assets/amikom.png')
+            ],
+            [
+                'name' => 'Midtrans',
+                'logo_url' => 'https://via.placeholder.com/150?text=Midtrans',
+            ],
+            [
+                'name' => 'Dicoding',
+                'logo_url' => 'https://via.placeholder.com/150?text=Dicoding',
+            ],
+            [
+                'name' => 'AWS Academy',
+                'logo_url' => 'https://via.placeholder.com/150?text=AWS',
+            ],
+            [
+                'name' => 'Gojek',
+                'logo_url' => 'https://via.placeholder.com/150?text=Gojek',
+            ]
+        ];
+
+        // Looping untuk memasukkan data dengan aman
+        foreach ($partners as $partner) {
+            Partner::updateOrCreate(
+                ['name' => $partner['name']], // Patokan pencarian: Nama Partner
+                ['logo_url' => $partner['logo_url']] // Data yang diupdate/diisi
+            );
+        }
     }
 }
