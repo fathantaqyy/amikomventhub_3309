7617b9cf2a1473f6349bba4d934abeb27b9cdcc0
fathantaqyy <fathantaqy@students.amikom.ac.id>
2026-05-25 18:25:56 +0700

    Tugas 8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -159,7 +159,7 @@
     
     <div class="text-center mb-14">
         <h2 class="text-4xl font-extrabold mb-4">
-            Partner AmikomEventHub keren
+            Partner AmikomEventHub
         </h2>
 
         <p class="text-slate-500 text-lg">
