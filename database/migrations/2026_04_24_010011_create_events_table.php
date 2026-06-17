5c4450e5067c13d1cc6e864ba91d0b25120fd0af
fathantaqyy <fathantaqy@students.amikom.ac.id>
2026-06-17 14:12:50 +0700

    Tugass 10

--- database/migrations/2026_04_24_010011_create_events_table.php
+++ database/migrations/2026_04_24_010011_create_events_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
 {
+    Schema::dropIfExists('events');
     Schema::create('events', function (Blueprint $table) {
         $table->id();
 
--- database/migrations/2026_05_23_110526_create_partners_table.php
+++ database/migrations/2026_05_23_110526_create_partners_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
 {
+    Schema::dropIfExists('partners');
     Schema::create('partners', function (Blueprint $table) {
         $table->id();
         $table->string('name');
